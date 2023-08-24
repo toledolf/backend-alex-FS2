14299019f587d30278cc84beb7f20f02fe9049fd
AlexSnider <alexsnider8@gmail.com>
2023-08-24 14:15:34 -0300

    1.0.3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import rotaTime from "./Rotas/rotaTime.js";
 /* import rotaTreinador from "./Rotas/rotaTreinador.js"; */
-import rotaUsuario from "./Rotas/rotaUsuario.js";
+/* import rotaUsuario from "./Rotas/rotaUsuario.js"; */
 
 const app = express();
 
@@ -15,15 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/time", rotaTime);
 /* app.use("/treinador", rotaTreinador); */
-app.use("/usuario", rotaUsuario);
+/* app.use("/usuario", rotaUsuario); */
 
 app.listen(port, host, () => {
   console.log(`API escutando no link: https://${host}/${port}/time`);
 });
 
-app.listen(port, host, () => {
+/* app.listen(port, host, () => {
   console.log(`API escutando no link: https://${host}/${port}/usuario`);
-});
+}); */
 
 /* app.listen(3007, "localhost", () => {
   console.log("API escutando no link: http://localhost:3006/treinador");
